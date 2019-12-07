da4b5ff0ddc93d8863b4566777d0a40d14604100
Philippe <phildoreau45@gmail.com>
2019-12-07 20:58:10 +0100

    Extract emzyme setup to setuTests.js

--- src/Congrats.test.js
+++ src/Congrats.test.js
@@ -1,12 +1,9 @@
 import React from 'react';
-import Enzyme, { shallow } from 'enzyme';
-import EnzymeAdapter from 'enzyme-adapter-react-16';
-import checkPropTypes from 'check-prop-types';
+import { shallow } from 'enzyme';
 
 import Congrats from './Congrats';
 import { findByTestAttr, checkProps } from "../test/testUtils";
 
-Enzyme.configure({ adapter: new EnzymeAdapter() });
 
 const defaultProps = { success: false };
 
