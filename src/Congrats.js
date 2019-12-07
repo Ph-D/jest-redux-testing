1e133348f2e290edc252b1f6fb482d6abd204f38
Philippe <phildoreau45@gmail.com>
2019-12-07 19:17:17 +0100

    congrats test

--- src/Congrats.js
+++ src/Congrats.js
@@ -3,11 +3,22 @@ import React from 'react';
 /**
 	* Function react component for congratulatory message
 	* @function
+	*@param {object} props - React props.
 	*@returns {JSX.Element} - Rendered component (or null if `success` prop is)
  */
 
-export default () => {
-	return (
-		<div></div>
-	)
+export default (props) => {
+	if (props.success) {
+		return (
+			<div data-test="component-congrats">
+				<span data-test="congrats-message">
+					Congratulations! You guess the word!
+				</span>
+			</div>
+		);
+	} else {
+		return (
+			<div data-test="component-congrats"></div>
+		);
+	}
 }
